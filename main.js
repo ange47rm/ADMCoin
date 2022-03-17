b4f52107d3bedc802332bdebad2d54a1b7184b78
Angelo Di Massimo <angelo.dimassimo@coopersoftware.co.uk>
2022-03-17 18:32:16 +0000

    Amended comments.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ class Block {
     // Timestamp: when the block was created
     // Data: any type of data (for currency it could be transaction details: money sent, sender, recipient)
     // PreviousHash: hash of the previous block 
+    // Nonce: Same properties within the block, will generate the same hash. When mining a new block, the nonce number allows the calculated hash value to always be different as it increases.
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
@@ -23,8 +24,6 @@ class Block {
     // If difficulty is set to 5, it means that the loop will run until the hash of the new block begins with 5 '0's and so on.
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
-            // With the same properties within the block, the calculated hash value will always be the same.
-            // The nonce number allows the calculated hash value to always be different.
             this.nonce++;
             this.hash = this.calculateHash();
         }
@@ -47,15 +46,15 @@ class Blockchain {
     }
 
     getLatestBlock() {
-        return this.chain[this.chain.length -1];
+        return this.chain[this.chain.length - 1];
     }
 
-    addBlock (newBlock) {
+    addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
-    
+
     // After a block is added it cannot be changed without invalidating the rest of the chain. 
     // Here we put some checks in place to verify the validity of the blockchain.
     isChainValid() {
@@ -89,10 +88,10 @@ let admCoin = new Blockchain();
 console.log(JSON.stringify(admCoin, null, 4));
 
 console.log(`Mining block #${admCoin.getNewBlockIndex()}`);
-admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '08/05/1991', { sender: 'Mario', recipient: 'Luigi', amount: 47}));
+admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '08/05/1991', { sender: 'Mario', recipient: 'Luigi', amount: 47 }));
 
 console.log(`Mining block #${admCoin.getNewBlockIndex()}`);
-admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '10/05/1991', { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 74}));
+admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '10/05/1991', { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 74 }));
 
 // TESTS
 console.log(JSON.stringify(admCoin, null, 4));
@@ -104,7 +103,7 @@ console.log(JSON.stringify(admCoin, null, 4));
 // console.log("Changed block #1's data and recalculated hash.");
 // admCoin.chain[1].data = { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 1074};
 // admCoin.chain[1].hash = admCoin.chain[1].calculateHash();
-// // Block 2 will have a previousHash value that'll be different from the recalculated one. 
+// // Block 2 will have a previousHash value that'll be different from the recalculated one.
 
 // // Verify blockchain validity.
 // console.log('Is blockchain valid? ', admCoin.isChainValid());
