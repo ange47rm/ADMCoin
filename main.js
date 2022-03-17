49ff18a46b82e9225cafa3afd1276151f9f09499
Angelo Di Massimo <angelo.dimassimo@coopersoftware.co.uk>
2022-03-17 18:26:56 +0000

    A new block is no longer simply just added. It now needs to be "mined". Introduced difficulty value for blockchain.

--- main.js
+++ main.js
@@ -12,17 +12,33 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    // The below methods defines how much computing power is required to create a new block.
+    // If difficulty is set to 5, it means that the loop will run until the hash of the new block begins with 5 '0's and so on.
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            // With the same properties within the block, the calculated hash value will always be the same.
+            // The nonce number allows the calculated hash value to always be different.
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined: ' + this.hash);
     }
 }
 
+// Potential issue: if data in a block is changed, hashes of all blocks could be recalculated and the blockchain would be valid.
+// Proof-of-work/mining can prevent that from happening by delaying the calculation of blocks' hash values, implementing a "difficulty" value.
 class Blockchain {
 
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;    // Determines how "difficult"/fast new block can be added to the blockchain.
     }
 
     // The first block (Genesis block) must be created manually
@@ -36,7 +52,7 @@ class Blockchain {
 
     addBlock (newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
     
@@ -57,28 +73,39 @@ class Blockchain {
         }
         return true;
     }
+
+    getNewBlockIndex() {
+        return this.chain.length;
+    }
 }
 
 // TODO
 // Implement a mechanism to rollback to a valid state, if the blockchain becomes invalid
 // This can happen when adding a new block the wrong way potentially / adding a new block / tampering with existing block data.
 
+
 let admCoin = new Blockchain();
-admCoin.addBlock(new Block(1, '08/05/1991', { sender: 'Mario', recipient: 'Luigi', amount: 47}));
-admCoin.addBlock(new Block(2, '10/05/1991', { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 74}));
+
+console.log(JSON.stringify(admCoin, null, 4));
+
+console.log(`Mining block #${admCoin.getNewBlockIndex()}`);
+admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '08/05/1991', { sender: 'Mario', recipient: 'Luigi', amount: 47}));
+
+console.log(`Mining block #${admCoin.getNewBlockIndex()}`);
+admCoin.addBlock(new Block(admCoin.getNewBlockIndex(), '10/05/1991', { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 74}));
 
 // TESTS
 console.log(JSON.stringify(admCoin, null, 4));
 
-// Verify blockchain validity.
-console.log('Is blockchain valid? ', admCoin.isChainValid());
+// // Verify blockchain validity.
+// console.log('Is blockchain valid? ', admCoin.isChainValid());
 
-// Tamper with block #1 by changing data in one block.
-console.log("Changed block #1's data and recalculated hash.");
-admCoin.chain[1].data = { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 1074};
-admCoin.chain[1].hash = admCoin.chain[1].calculateHash();
-// Block 2 will have a previousHash value that'll be different from the recalculated one. 
+// // Tamper with block #1 by changing data in one block.
+// console.log("Changed block #1's data and recalculated hash.");
+// admCoin.chain[1].data = { sender: 'Scorpion', recipient: 'Sub-Zero', amount: 1074};
+// admCoin.chain[1].hash = admCoin.chain[1].calculateHash();
+// // Block 2 will have a previousHash value that'll be different from the recalculated one. 
 
-// Verify blockchain validity.
-console.log('Is blockchain valid? ', admCoin.isChainValid());
+// // Verify blockchain validity.
+// console.log('Is blockchain valid? ', admCoin.isChainValid());
 
